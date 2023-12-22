5e2025d7045335b24b7dcc494f923c34c1aaff6a
Marcos <marcsalexandr@gmail.com>
2023-12-22 18:45:37 -0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Store
+
+
+
+
+# AngularStore
+
+![Imagem do Projeto](readme.jpg)
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.7.
 
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent implements OnInit{
+
+
+
+	@Input()
+	gameLabel:string = ""
+  @Input()
+  gameCover:string = ""
+  @Input()
+  gameType:string="Digital PS4"
+  @Input()
+  gamePrice:string="R$399,90"
+
+  constructor(){}
+  ngOnInit(): void {
+
+  }
+
+}
